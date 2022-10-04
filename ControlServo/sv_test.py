831057afc487a4e2e06ea8b61f24a2c84efdb338
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-04 14:43:32 +0900

    feat : servo code

--- ControlServo/sv_test.py
+++ ControlServo/sv_test.py
@@ -2,6 +2,7 @@
 # 각도 값을 변환해서 아예 그거로 써도 될 것 같다.
 # 시그모이드 함수
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
@@ -11,11 +12,28 @@ pos_center = 7.5 #<- fill this using the answer for Question 3.
 pwm = GPIO.PWM(17,50)  # (channel, frequency)
 pwm.start(pos_center)
 
+FLAG = True
+curr_pos = pos_center
+
 while True:
     try:
-        raw_input = input("Duty (in %):")
-        duty = float(raw_input)
-        pwm.ChangeDutyCycle(duty)
+        #raw_input = input("Duty (in %):")
+        # duty = float(raw_input)
+        # pwm.ChangeDutyCycle(duty)
+        time.sleep(0.01)
+        if FLAG:
+            if curr_pos >= 12:
+                FLAG = False
+                continue
+            curr_pos= curr_pos + 0.1
+        else:
+            if curr_pos <= 3:
+                FLAG = True
+                continue
+            curr_pos = curr_pos - 0.1
+        print(curr_pos)
+        print(FLAG)
+        pwm.ChangeDutyCycle(float(curr_pos))
 	
     except ValueError:
     	print("Please enter a real number in percent.")
